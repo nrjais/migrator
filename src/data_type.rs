a93d71bec6261f69ffe096fe8f7f38f27437634a
Neeraj Jaiswal <neerajj85@gmail.com>
2020-09-10 10:11:33 +0530

    Initial commit

---
+++ src/data_type.rs
@@ -0,0 +1,9 @@
+enum BaseType {
+    Integer(u32),
+    VarChar(u32),
+}
+
+struct DataType {
+    base_type: BaseType,
+    
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+pub(crate) mod data_type;
+pub(crate) mod migration;
+
+fn main() {
+    println!("Hello, world!");
+}
