e4bab0c2ae458e0840c09467a762d088fa4c74a0
Neeraj Jaiswal <neerajj85@gmail.com>
2020-09-17 10:32:27 +0530

    Add deserialization of tooml migration file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use migrations::{ChangeLog, Migration};
+use std::{error::Error, fs};
+
 pub(crate) mod migrations;
 
-fn main() {
-    println!("Hello world");
+const FILE_PATH: &'static str = "examples/migrations/changelog.toml";
+const MIGRATION_PATH: &'static str = "examples/migrations/changes/create_table.toml";
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = fs::read(FILE_PATH)?;
+
+    let change_log: ChangeLog = toml::from_slice(&file)?;
+    println!("change_log: {:?}", change_log);
+
+    let file = fs::read(MIGRATION_PATH)?;
+    let migration: Migration = toml::from_slice(&file)?;
+    println!("migration: {:?}", migration);
+
+    Ok(())
 }
--- src/migrations.rs
+++ src/migrations.rs
@@ -1,3 +1,38 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
 pub struct ChangeLog {
-  
+    include: Vec<IncludePath>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct IncludePath {
+    path: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Migration {
+    id: u32,
+    change: Vec<ChangeSet>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ChangeSet {
+    dialect: Option<String>,
+    up: Change,
+    down: Option<Change>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+pub enum Change {
+    Query { query: String },
+    Queries { queries: Vec<String> },
+    SqlFile { sql_file: SqlFile },
+}
+
+#[derive(Deserialize, Debug)]
+pub struct SqlFile {
+    path: String,
+    new_line_delimited: bool,
 }
