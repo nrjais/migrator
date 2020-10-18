da9bc3e0669fb8f72fe434d209d0b9f6d6d9f7bc
Neeraj Jaiswal <neerajj85@gmail.com>
2020-10-18 12:03:15 +0530

    Add migration entry after a changeset is commited

--- src/backend/postgres.rs
+++ src/backend/postgres.rs
@@ -1,7 +1,7 @@
 use super::migration_column_names;
-use crate::executor::{Backend, DBMigration};
+use crate::executor::{Backend, DBMigration, MigrationEntry};
 use crate::Result;
-use migration_column_names::ID;
+use migration_column_names::{DESC, ID};
 use postgres::{Client, NoTls, Row};
 
 pub struct PostgresBackend {
@@ -25,16 +25,21 @@ impl PostgresBackend {
 
 const SELECT_ALL_MIGRATIONS_QUERY: &'static str = "SELECT ID FROM DB_CHANGELOG ORDER BY ID;";
 const CHANGELOG_TABLE_CREATION_QUERY: &'static str = "
-      CREATE TABLE IF NOT EXISTS DB_CHANGELOG (
-        EXECUTION_ORDER serial NOT NULL,
-        ID INT PRIMARY KEY NOT NULL,
-        created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
-      );
+  CREATE TABLE IF NOT EXISTS DB_CHANGELOG (
+    ID BIGINT PRIMARY KEY NOT NULL,
+    EXECUTION_ORDER SERIAL NOT NULL,
+    DESCRIPTION VARCHAR(15) NOT NULL,
+    CREATED_ON TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+  );
+";
+const INSERT_MIGRATION_ENTRY_QUERY: &'static str = "
+  INSERT INTO DB_CHANGELOG(ID, DESCRIPTION) VALUES ($1, $2);
 ";
 
 fn migration_from(row: Row) -> Result<DBMigration> {
     Ok(DBMigration {
         id: row.try_get(ID)?,
+        desc: row.try_get(DESC)?,
     })
 }
 
@@ -55,11 +60,14 @@ impl Backend for PostgresBackend {
         Ok(vec![])
     }
 
-    fn in_transaction(&mut self, queries: Vec<String>) -> Result<()> {
+    fn in_transaction(&mut self, queries: Vec<String>, entry: MigrationEntry) -> Result<()> {
         let mut transaction = self.client.transaction()?;
         for query in queries.iter() {
             transaction.execute(query.as_str(), &[])?;
         }
+
+        transaction.execute(INSERT_MIGRATION_ENTRY_QUERY, &[&entry.id, &entry.desc])?;
+
         transaction.commit()?;
         Ok(())
     }
--- src/executor/mod.rs
+++ src/executor/mod.rs
@@ -8,13 +8,24 @@ mod planner;
 
 pub struct DBMigration {
     pub id: i64,
+    pub desc: String,
+}
+
+pub mod migration_entry_desc {
+    pub const QUERY: &'static str = "query";
+    pub const QUERY_LIST: &'static str = "query_list";
+}
+
+pub struct MigrationEntry {
+    pub id: i64,
+    pub desc: String,
 }
 
 pub trait Backend {
     const CHANGELOG_TABLE_CREATION_QUERY: &'static str;
     fn execute(&mut self, query: String) -> Result<()>;
     fn db_migrations(&mut self) -> Result<Vec<DBMigration>>;
-    fn in_transaction(&mut self, queries: Vec<String>) -> Result<()>;
+    fn in_transaction(&mut self, queries: Vec<String>, entry: MigrationEntry) -> Result<()>;
 
     fn init(&mut self) -> Result<()> {
         self.execute(Self::CHANGELOG_TABLE_CREATION_QUERY.into())
@@ -30,11 +41,19 @@ impl<T: Backend> Executor<T> {
         Self { backend }
     }
 
-    fn execute(&mut self, migration: Migration) -> Result<()> {
+    fn apply(&mut self, migration: Migration) -> Result<()> {
         for change_set in migration.changes.into_iter() {
             match change_set.up {
-                Change::Query { query } => self.apply_changes(vec![query])?,
-                Change::Queries { queries } => self.apply_changes(queries)?,
+                Change::Query { query } => self.apply_changes(
+                    vec![query],
+                    migration_entry_desc::QUERY.into(),
+                    migration.id,
+                )?,
+                Change::Queries { queries } => self.apply_changes(
+                    queries,
+                    migration_entry_desc::QUERY_LIST.into(),
+                    migration.id,
+                )?,
                 Change::SqlFile { .. } => todo!(),
             }
         }
@@ -42,8 +61,9 @@ impl<T: Backend> Executor<T> {
         Ok(())
     }
 
-    fn apply_changes(&mut self, queries: Vec<String>) -> Result<()> {
-        self.backend.in_transaction(queries)
+    fn apply_changes(&mut self, queries: Vec<String>, desc: String, id: i64) -> Result<()> {
+        self.backend
+            .in_transaction(queries, MigrationEntry { id, desc })
     }
 
     pub fn init(&mut self) -> Result<()> {
@@ -57,7 +77,7 @@ impl<T: Backend> Executor<T> {
         for p_migration in planned_migrations.into_iter() {
             match p_migration {
                 MigrationPlan::Pending(migration) => {
-                    self.execute(migration)?;
+                    self.apply(migration)?;
                 }
             }
         }
